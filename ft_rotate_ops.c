a65d4d813d9ff50dbf0f9fc4e422e2b34b8bc71b
p1nh4 <davidos-@student.42porto.com>
2026-04-29 21:10:46 +0100

    feat: add rotate operations (ra, rb, rr)

--- ft_rotate_ops.c
+++ ft_rotate_ops.c
@@ -6,36 +6,55 @@
 /*   By: davidos- <davidos-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/25 19:12:29 by davidos-          #+#    #+#             */
-/*   Updated: 2026/04/25 20:02:27 by davidos-         ###   ########.fr       */
+/*   Updated: 2026/04/29 21:09:43 by davidos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include <unistd.h>
 
-
-int	ft_lstsize(t_stack *lst)
+t_stack	*ft_lstnode(t_stack *lst)
 {
-	int		len;
 	t_stack	*node;
 
-	len = 0;
 	node = lst;
 	while (node != ((void *)0))
 	{
-		len++;
+		lst = node;
 		node = node->next;
 	}
-	return (len);
+	return (lst);
 }
 
 void	ft_rotate(t_stack **a)
 {
-	int		size;
+	t_stack	*last_node;
 	t_stack	*temp;
 
+	if (!*a || (*a)->next == ((void *)0))
+		return ;
+	last_node = ft_lstnode(*a);
 	temp = *a;
-	size = ft_lstsize(*a);
+	*a = (*a)->next;
+	last_node->next = temp;
+	temp->next = ((void *)0);
+}
 
+void	ft_ra(t_stack **a)
+{
+	ft_rotate(a);
+	write(1, "ra\n", 3);
+}
 
+void	ft_rb(t_stack **b)
+{
+	ft_rotate(b);
+	write(1, "rb\n", 3);
+}
 
+void	ft_rr(t_stack **a, t_stack **b)
+{
+	ft_rotate(a);
+	ft_rotate(b);
+	write(1, "rr\n", 3);
 }
